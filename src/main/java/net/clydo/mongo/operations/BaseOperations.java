5431a21d66dced481c8130bfcd69c2254acaac7e
RezaNajafian <reza.najafian.rena@gmail.com>
2025-12-08 13:02:19 +0330

    refactor(base-operations): add getter for collection field

--- src/main/java/net/clydo/mongo/operations/BaseOperations.java
+++ src/main/java/net/clydo/mongo/operations/BaseOperations.java
@@ -21,6 +21,7 @@
 package net.clydo.mongo.operations;
 
 import com.mongodb.client.MongoCollection;
+import lombok.Getter;
 import lombok.val;
 import net.clydo.mongo.meta.model.ModelMeta;
 import net.clydo.mongo.meta.model.index.IndexMetaMap;
@@ -29,6 +30,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class BaseOperations<M> {
 
+    @Getter
     protected final MongoCollection<M> collection;
     protected final ModelMeta<M> model;
     protected final IndexMetaMap indices;
